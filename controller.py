95b42cb25fbc5a0cc1a4533dee987fa9b843e0b9
Юлия Викторовна <julsbi@bk.ru>
2023-08-14 01:20:55 +0300

    corrected

--- controller.py
+++ controller.py
@@ -10,52 +10,13 @@ def start():
             case 1:
                 model.add_note()
                 view.print_message(text.add_successful)
-            # case 2:
-            # model.open_pb()    
-            # model.save_pb()
-            #     view.print_message(text.save_successful)
-            #case 3:
-            #    model.read_notes()
-            #    view.print_message(text.)
-            case 4:
+            case 2:
+                model.view_notes()    
+            case 3:
                 model.edit_note()
                 view.print_message(text.edit_successful)
-            #     view.print_message(text.new_contact_successful(name))
-            # case 5:
-            #     key_word=view.input_search()
-            #     result=model.search_contact(key_word)
-            #     view.print_contacts(result, text.empty_search(key_word))
-            # case 6:
-            #     key_word=view.input_search(text.input_change)
-            #     result=model.search_contact(key_word)
-            #     if result:
-            #         if len(result) !=1:
-            #             view.print_contacts(result, ' ')
-            #             current_id=view.input_search(text.input_index)
-
-            #         else:
-            #             current_id=result[0].get('id')
-            #         new_contact=view.input_contact(text.change_contact)
-            #         name=model.change_contact(new_contact, current_id)
-            #         view.print_message(text.change_successful(name))
-            #     else:
-            #         view.print_message(text.change_successful(name))
-
-            # case 7:
-            #     key_word=view.input_search(text.input_delete)
-            #     result=model.search_contact(key_word)
-            #     if result:
-            #         if len(result) !=1:
-            #             view.print_contacts(result, ' ')
-            #             current_id=view.input_search(text.input_index)
-
-            #         else:
-            #             current_id=result[0].get('id')
-            #         model.delete_contact(result)
-            #         view.print_message(text.delete_successful)
-            #     else:
-            #         view.print_message(text.delete_successful)
-
-
-            case 7:
+            case 4:
+                model.delete_note()
+                view.print_message(text.delete_successful)
+            case 5:
                 break    
--- model.py
+++ model.py
@@ -49,6 +49,33 @@ def edit_note():
     else:
         print ('Заметка не найдена')
 
+# Функция удаления заметки
+def delete_note():
+    id = input("Введите идентификатор заметки для удаления: ")
+    note=next((note for note in notes if note.id ==id), None)
+    if note:
+        notes.remove(note)
+        save_notes(notes)
+    else:
+        print ('Заметка не найдена')
+
+# Функция вывода списка заметок и фильтрация по дате
+def view_notes():
+    date_str= input('Введите дату для фильтрации заметок в формате dd.mm.yyyy: ')
+    try:
+        filter_date=datetime.strptime(date_str, '%d.%m.%Y')
+        filtered_notes= [note for note in notes if datetime.strptime(note.date, '%d.%m.%Y %H:%M:%S').date() == filter_date.date()]
+    except ValueError:
+        filtered_notes = notes
+    if filtered_notes:
+        for note in filtered_notes:
+            print(f'{note.id}. {note.title}: {note.body} - ({note.date})')
+    else:
+        print ('Нет заметок для отображения')
+
+
+
+
 
 
 
